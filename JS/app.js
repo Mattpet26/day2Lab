9a13f7150925c9a1722ea92a28a800f330b93ef7
Matthew <Mattpet26@gmail.com>
2020-08-12 08:56:52 -0700

    working on task 2 still

--- JS/app.js
+++ JS/app.js
@@ -1,12 +1,14 @@
 'use strict';
 
 const imageArray = [];
+const KeyWordArray =[];
 
-function Gallery (name, src, about, keyword){
+function Gallery (name, src, about, keyword, horns){
   this.name = name;
   this.src = src;
   this.about = about;
   this.keyword = keyword;
+  this.horns = horns;
 
   imageArray.push(this)
 }
@@ -16,13 +18,11 @@ Gallery.prototype.renderJQuery = function(){
   // replace the values
   // put it where you want
   const $clonedListItemElement = $('li:first-child').clone();
-  console.log($clonedListItemElement);
 
   $clonedListItemElement.find('h2').text(this.name);
   $clonedListItemElement.find('img').attr('src', this.src).attr('about', this.about);
   $clonedListItemElement.find('p').text(this.about);
   $clonedListItemElement.find('#keywordsGo').text(this.keyword);
-
 
   $('ul').append($clonedListItemElement);
 }
@@ -35,37 +35,45 @@ const optionObject = {
 }
 
 // reconstitute - pass it(back) trhough the constructor
-const handleThePetsFromFile = dataPotato => {
-  console.log(dataPotato);
-  dataPotato.forEach(newImages => {
-    new Gallery (newImages.title, newImages.image_url, newImages.description, newImages.keyword);
+const handleImagesFromJson = ArrFromJson => {
+  ArrFromJson.forEach(newImages => {
+    new Gallery (newImages.title, newImages.image_url, newImages.description, newImages.keyword, newImages.horns);
+
+    if (!KeyWordArray.includes(newImages.keyword)) {
+      KeyWordArray.push(newImages.keyword);
+
+    }
   });
   imageArray.forEach(petValue => petValue.renderJQuery());
-  const keyWordHolder = this.keyword;
+  renderOptionSelection(KeyWordArray);
 };
 $.ajax('page-1.json', optionObject)
-  .then(handleThePetsFromFile);
+  .then(handleImagesFromJson);
 
-function keyWordFilter(){
-  let selectKeyword = $(this).val();
-    $('h2').hide();
-    $('img').hide();
-    $('p').hide();
-    console.log(imageArray);
+const renderOptionSelection = function (KeyWordArray) {
+  KeyWordArray.forEach(keyword => {
+    const $clonedOption = $('option:first-child').clone();
+    $clonedOption.attr('value', keyword);
+    $clonedOption.text(keyword);
+    $('select').append($clonedOption);
+  });
+};
+
+$('select').on('change', selectOptionKeyWord);
+
+function selectOptionKeyWord (){
+  const userSelection = $(this).val();
+  $('li').hide();
+  imageArray.forEach(function(storedImages){
+    if(userSelection === storedImages.keyword){
+      const imageTitle = storedImages.title;
+      const imageListItems = $('li');
+      imageListItems.each(function(){
+        console.log(imageTitle)
+        if($(this).find('h2').text() === imageTitle){
+          $(this).show();
+        }
+      })
+    }
+  })
 }
-$('#selectKeyword').on('change', keyWordFilter);
-
-//step 1.5 we need to categorize each image 
-// imageArray needs to be checked if they match the keyword
-function SortImagesToCategories (){
-  if(this.keyword === 'narwal'){
-
-// function DisplaySelectedItem(){
-//   if(this.keyword === 'narwhal'){
-//     $('h2').text(this.title);
-//     $('img').attr(this.src);
-//     $('p').text(this.description);
-//     console.log('narhwals');
-//   }
-// }
-// $('selectKeyword').on('change', DisplaySelectedItem);
