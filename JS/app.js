b683aba16993587e832b6739663a93b9a73f7bbf
Matthew <Mattpet26@gmail.com>
2020-08-12 15:19:04 -0700

    added horn feature, fixxed duplicate keyword

--- JS/app.js
+++ JS/app.js
@@ -1,7 +1,8 @@
 'use strict';
 
-const imageArray = [];
-const KeyWordArray =[];
+let imageArray = [];
+let KeyWordArray = [];
+let hornsArray = [];
 
 function Gallery (name, src, about, keyword, horns){
   // name, src, about, ect are parameters
@@ -11,7 +12,8 @@ function Gallery (name, src, about, keyword, horns){
   this.keyword = keyword;
   this.horns = horns;
 
-  imageArray.push(this)
+  hornsArray.push(this);
+  imageArray.push(this);
 }
 
 Gallery.prototype.renderWithCreation = function(){
@@ -23,20 +25,7 @@ Gallery.prototype.renderWithCreation = function(){
   $('ul').prepend($newListItem)
 }
 
-Gallery.prototype.renderJQuery = function(){
-  // get a clone of a pre-existing good html copy
-  // replace the values
-  // put it where you want
-  const $clonedListItemElement = $('li:first-child').clone();
 
-  $clonedListItemElement.find('h2').text(this.name);
-  $clonedListItemElement.find('img').attr('src', this.src).attr('about', this.about);
-  $clonedListItemElement.find('p').text(this.about);
-  $clonedListItemElement.find('#keywordsGo').text(this.keyword);
-
-  $('ul').append($clonedListItemElement);
-  $clonedListItemElement.show();
-}
 
 const optionObject = {
   // this is an object literal with properties
@@ -44,36 +33,55 @@ const optionObject = {
   datatype: 'json'
 }
 
+
 const handleImagesFromJson = ArrFromJson => {
   // the thing before an arrow function is the parameter = (arrfromjson)
   ArrFromJson.forEach(newImages => {
     new Gallery (newImages.title, newImages.image_url, newImages.description, newImages.keyword, newImages.horns);
 
+    //enter a sort statement here
+    if (!hornsArray.includes(newImages.horns)) {
+      
+    }
+
     if (!KeyWordArray.includes(newImages.keyword)) {
       KeyWordArray.push(newImages.keyword);
-
     }
   });
   imageArray.forEach(petValue => petValue.renderMustache());
   renderOptionSelection(KeyWordArray);
+  renderOptionsForHorns(hornsArray)
 };
 $.ajax('data/page-1.json', optionObject)
   .then(handleImagesFromJson);
 
-const renderOptionSelection = function (KeyWordArray) {
-  KeyWordArray.forEach(keyword => {
-    const $clonedOption = $('option:first-child').clone();
+
+
+
+
+const renderOptionsForHorns = function (hornsArray) {
+  hornsArray.forEach(horns => {
+    const $clonedOption = $('<option></option>').clone();
+    $clonedOption.attr('value', horns);
+    $clonedOption.text(horns);
+    $('#selectHorns').append($clonedOption);
+  });
+};
+
+const renderOptionSelection = function (Potato) {
+  Potato.forEach(keyword => {
+    const $clonedOption = $('<option></option>');
     $clonedOption.attr('value', keyword);
     $clonedOption.text(keyword);
-    $('select').append($clonedOption);
+    $('#selectKeyword').append($clonedOption);
   });
 };
 
 
-$('select').on('change', selectOptionKeyWord);
+
 // chosing a select, then when the event change happens we run the funct
 // on == ELEMENT.addEventListener
-
+$('select').on('change', selectOptionKeyWord);
 function selectOptionKeyWord (){
   const userSelection = $(this).val();
   $('li').hide();
@@ -98,11 +106,24 @@ function selectOptionKeyWord (){
   show that list item
 */
 
+
+
 Gallery.prototype.renderMustache = function() {
   const newHtml = Mustache.render($('#image-Template').html(), this);
   $('ul').append(newHtml)
 }
 
 
+
+function renderNewPageImages (){
+  $('li').hide();
+  $('option').hide();
+  $('option:first-child').show();
+  imageArray = [];
+  $.ajax('data/page-2.json', optionObject)
+  .then(handleImagesFromJson);
+}
+
+$('button').on('click', renderNewPageImages)
 
 $('li:first-child').hide();
