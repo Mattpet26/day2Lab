bb4252a5161391d25a6b2b3baa35eb81023c28af
Matthew <Mattpet26@gmail.com>
2020-08-11 17:58:38 -0700

    added hide feature

--- JS/app.js
+++ JS/app.js
@@ -2,10 +2,11 @@
 
 const imageArray = [];
 
-function Gallery (name, src, about){
+function Gallery (name, src, about, keyword){
   this.name = name;
   this.src = src;
   this.about = about;
+  this.keyword = keyword;
 
   imageArray.push(this)
 }
@@ -20,13 +21,11 @@ Gallery.prototype.renderJQuery = function(){
   $clonedListItemElement.find('h2').text(this.name);
   $clonedListItemElement.find('img').attr('src', this.src).attr('about', this.about);
   $clonedListItemElement.find('p').text(this.about);
+  $clonedListItemElement.find('#keywordsGo').text(this.keyword);
+
 
   $('ul').append($clonedListItemElement);
 }
-
-
-new Gallery('Max', 'max.jpg', 'Max is a cool kat');
-
 
 // asynchronous javascript and xml - million years to complete
 // javascript does it asynchronously - does it in background
@@ -38,14 +37,35 @@ const optionObject = {
 // reconstitute - pass it(back) trhough the constructor
 const handleThePetsFromFile = dataPotato => {
   console.log(dataPotato);
-  dataPotato.forEach(lamePet => {
-    new Gallery (lamePet.name, lamePet.src, lamePet.about);
+  dataPotato.forEach(newImages => {
+    new Gallery (newImages.title, newImages.image_url, newImages.description, newImages.keyword);
   });
   imageArray.forEach(petValue => petValue.renderJQuery());
+  const keyWordHolder = this.keyword;
 };
-$.ajax('data/page-1.json', optionObject)
+$.ajax('page-1.json', optionObject)
   .then(handleThePetsFromFile);
 
+function keyWordFilter(){
+  let selectKeyword = $(this).val();
+    $('h2').hide();
+    $('img').hide();
+    $('p').hide();
+    console.log(imageArray);
+}
+$('#selectKeyword').on('change', keyWordFilter);
 
+//step 1.5 we need to categorize each image 
+// imageArray needs to be checked if they match the keyword
+function SortImagesToCategories (){
+  if(this.keyword === 'narwal'){
 
-imageArray.forEach(petValue => petValue.renderJQuery());
+// function DisplaySelectedItem(){
+//   if(this.keyword === 'narwhal'){
+//     $('h2').text(this.title);
+//     $('img').attr(this.src);
+//     $('p').text(this.description);
+//     console.log('narhwals');
+//   }
+// }
+// $('selectKeyword').on('change', DisplaySelectedItem);
